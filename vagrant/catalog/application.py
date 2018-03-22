17ef68c6ceac74442dfb56c5d8c3216009d0ea9c
Michael Werts <8486457+mcwerts@users.noreply.github.com>
2018-03-21 22:30:47 -0700

    Message flashing working

--- vagrant/catalog/application.py
+++ vagrant/catalog/application.py
@@ -1,7 +1,7 @@
 #!/usr/bin/env python3
 """Simple catalog app"""
 
-from flask import Flask, render_template, request, redirect, url_for, jsonify
+from flask import Flask, render_template, request, redirect, url_for, flash, jsonify
 from sqlalchemy import create_engine
 from sqlalchemy.orm import sessionmaker
 from database_setup import Base, Category, Item
@@ -16,6 +16,11 @@ session = DBSession()
 
 
 def lookupJoinTupleByName(category_name, item_name):
+  """
+  Search inner join of Category and Item tables for category_name and item_name.
+
+  Return (Category, Item) tuple.
+  """
   result = session.query(Category, Item).filter(Category.id==Item.category_id).\
     filter(Category.name==category_name).filter(Item.name==item_name).one()
   return result
@@ -55,7 +60,7 @@ def showItems(category_name):
 @app.route('/catalog/<string:category_name>/<string:item_name>/')
 def showItemDetails(category_name, item_name):
   join_tuple = lookupJoinTupleByName(category_name, item_name)
-  print(item)
+  #print(item)
   return render_template('itemDetails.html',
     category_name=category_name, item_name=item_name, item=join_tuple)
 
@@ -73,6 +78,8 @@ def addItem():
     session.add(newItem)
     session.commit()
 
+    flash("Item added: {} / {}".format(category, newItem.name))
+
     return redirect(url_for('showCatalog'))
   else:
     categories = session.query(Category).all()
@@ -81,16 +88,23 @@ def addItem():
 
 @app.route('/catalog/<string:category_name>/<string:item_name>/edit', methods = ['GET', 'POST'])
 def editItem(category_name, item_name):
-  item_tuple = lookupJoinTupleByName(category_name, item_name)
+  join_tuple = lookupJoinTupleByName(category_name, item_name)
   if request.method == 'POST':
-    edited_item = item_tuple[1]
+    edited_item = join_tuple[1]
+
     if request.form['name']:
+      name = request.form['name']
       edited_item.name = request.form['name']
+    else:
+      name = item_name
     if request.form['description']:
       edited_item.description = request.form['description']
+
+    category = request.form['category']
+    edited_item.category_id = session.query(Category.id).filter_by(name=request.form['category']).scalar()
     session.add(edited_item)
     session.commit()
-    #should we redirect to edited item?
+    flash("Item edited: {} / {}".format(category, name))
     return redirect(url_for('showCatalog'))
   else:
     item = item_tuple[1]
@@ -106,6 +120,7 @@ def deleteItem(category_name, item_name):
     item = join_tuple[1]
     session.delete(item)
     session.commit()
+    flash("Item deleted: {} / {}".format(category_name, item_name))
     return redirect(url_for('showCatalog'))
   else:
     return render_template('deleteItem.html', category_name=category_name, item_name=item_name)
@@ -117,6 +132,7 @@ def getItemJSON(category_name, item_name):
 
 
 if __name__ == '__main__':
+  app.secret_key = 'super_secret_key'  #TODO
   app.debug = True
   app.run(host = '0.0.0.0', port = 8000)
 
